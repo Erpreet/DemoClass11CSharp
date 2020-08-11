8be56f75987ca46010b21c2af480e8f4f2deb71c
Erpreet <preetkaurchd15@gmail.com>
2020-08-11 09:53:03 -0600

    InitialCommit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace FirstDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name; //Declare(create) a variable using the datatype followd by the name
+            name = "Preet";// Assign a value to variable
+
+            Console.ReadLine();
+
+            Console.WriteLine(name); //Retrieve value from variable and send it to console
+        }
+    }
+}
