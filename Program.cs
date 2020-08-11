2d45c8d0ce0cd58c70d06383e9984940ae313202
Erpreet <preetkaurchd15@gmail.com>
2020-08-11 10:36:16 -0600

    Birth Year

--- Program.cs
+++ Program.cs
@@ -7,16 +7,32 @@ namespace FirstDemo
         static void Main(string[] args)
         {
             //Declare(create) a variable using the datatype followd by the name.
-            
-            string name; 
+
+            string text; //Strings store text 
+            char character; //stores a single character (including space, newline, etc)
+            int wholeNumber; // Int stores a number without a decimal point (positive or negative) , byte, short and long are integer types with varying size limits
+            double number; // Double stores a number without a decimal point // float and decimal re alternatives with quirks
+            bool boolean; // Bool stores a true or false value
+
+
             Console.WriteLine("Please enter your name below"); //Tell the user what we are expecting
 
+            string name; // DEclare a variable
+            int age, birthyear;
+
+            Console.WriteLine("Please enter your name below");
             name = Console.ReadLine();// Assign a value to variable (that we get from user)
+
+            Console.WriteLine("Please enter your age below");
+            age = int.Parse(Console.ReadLine());
+
+            birthyear = 2020 - age;
 
 
             Console.WriteLine(name); //Retrieve value from variable and send it to console
 
-            Console.WriteLine("Hello," +name);
+            Console.WriteLine("Hello," +name + " . You were born in the year" +birthyear + "." );
+
 
         }
     }
